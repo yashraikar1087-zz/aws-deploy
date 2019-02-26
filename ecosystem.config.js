a768bf3d52991c1de12f43c6ab61373f1905e16f
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-26 15:39:53 -0700

    remove /

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-54-189-172-129.us-west-2.compute.amazonaws.com/",
+      host: "ec2-54-189-172-129.us-west-2.compute.amazonaws.com",
       key: "~/.ssh/tutorial.pem",
       ref: "origin/master",
       repo: "git@github.com:yashraikar1087/aws-deploy.git",
