c8f757d96e9a8a343cd64ed71ca4395659d54384
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-26 15:56:31 -0700

    change dir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: "tutorial-2",
-      script: "./index.js"
+      script: "./source/index.js"
     }
   ],
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-54-189-172-129.us-west-2.compute.amazonaws.com",
+      host: "ec2-54-189-172-129.us-west-2.compute.amazonaws.com/tutorial-2",
       key: "~/.ssh/tutorial.pem",
       ref: "origin/master",
       repo: "git@github.com:yashraikar1087/aws-deploy.git",
